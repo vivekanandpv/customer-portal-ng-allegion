035c92c3ccccad05959ed2349656f9727de2d51f
Vivekanand P V <vivekanandpv@outlook.com>
2022-09-28 12:37:51 +0530

    implements customer details

--- src/app/customer-detail/customer-detail.component.ts
+++ src/app/customer-detail/customer-detail.component.ts
@@ -1,15 +1,24 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Observable } from 'rxjs';
+import { RestService } from '../rest.service';
+import { CustomerViewModel } from '../_models/app.models';
 
 @Component({
   selector: 'app-customer-detail',
   templateUrl: './customer-detail.component.html',
-  styleUrls: ['./customer-detail.component.scss']
+  styleUrls: ['./customer-detail.component.scss'],
 })
 export class CustomerDetailComponent implements OnInit {
+  customerId: number;
+  customer$: Observable<CustomerViewModel>;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private ar: ActivatedRoute, private restService: RestService) {
+    this.customerId = +(this.ar.snapshot.paramMap.get('id') ?? '');
+    this.customer$ = this.restService.read<CustomerViewModel>(
+      `customers/${this.customerId}`
+    );
   }
 
+  ngOnInit(): void {}
 }
