cef92245fabc14be4e8fa2e7c3cda524e87588fd
Vivekanand P V <vivekanandpv@outlook.com>
2022-09-28 12:08:15 +0530

    implements account details

--- src/app/account-detail/account-detail.component.ts
+++ src/app/account-detail/account-detail.component.ts
@@ -1,15 +1,24 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Observable } from 'rxjs';
+import { RestService } from '../rest.service';
+import { AccountViewModel } from '../_models/app.models';
 
 @Component({
   selector: 'app-account-detail',
   templateUrl: './account-detail.component.html',
-  styleUrls: ['./account-detail.component.scss']
+  styleUrls: ['./account-detail.component.scss'],
 })
 export class AccountDetailComponent implements OnInit {
+  accountId: number;
+  account$: Observable<AccountViewModel>;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private ar: ActivatedRoute, private restService: RestService) {
+    this.accountId = +(this.ar.snapshot.paramMap.get('id') ?? '');
+    this.account$ = this.restService.read<AccountViewModel>(
+      `accounts/${this.accountId}`
+    );
   }
 
+  ngOnInit(): void {}
 }
--- src/app/account-list/account-list.component.ts
+++ src/app/account-list/account-list.component.ts
@@ -1,15 +1,19 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { RestService } from '../rest.service';
+import { AccountViewModel } from '../_models/app.models';
 
 @Component({
   selector: 'app-account-list',
   templateUrl: './account-list.component.html',
-  styleUrls: ['./account-list.component.scss']
+  styleUrls: ['./account-list.component.scss'],
 })
 export class AccountListComponent implements OnInit {
+  accounts$: Observable<AccountViewModel[]>;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private restService: RestService) {
+    this.accounts$ = this.restService.read<AccountViewModel[]>('accounts');
   }
 
+  ngOnInit(): void {}
 }
